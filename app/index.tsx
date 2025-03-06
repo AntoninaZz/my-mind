7ae5e19aff119cabc94315af4f5b2f1e2a06901f
AntoninaZz <zdanton02@gmail.com>
2025-03-06 13:29:41 +0200

    add linear gradient

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      style={styles.container}
+      colors={['#43BCF0', '#541896', "#711280"]}
+      start={{ x: 1.2, y: 0.8 }}
+      end={{ x: 1, y: 1 }}
+
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Text>Home screen</Text>
+      <Link href="/level">
+        Go to About screen
+      </Link>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // background: linear-gradient(191.46deg, #43BCF0 -32.01%, #541896 55.91%, #711280 116.41%);
+
+  },
+  background: {
+    flex: 1,
+    position: 'absolute',
+    left: 0,
+    top: 0,
+  },
+});
